977301b348064ead55c356906c8fe5e75177bb2a
Dark System <darksystemcoder@gmail.com>
2023-10-15 19:21:50 -0400

    eod

--- mapGen.js
+++ mapGen.js
@@ -1,12 +1,13 @@
 const Jimp = require('jimp')
 const fs = require('fs')
+const yaml = require('js-yaml')
 async function main(){
 const image = await Jimp.read(process.argv[2]);
 var colors={grass:[0,171,61],snow:[255,255,255]}
 var img={}
-for(var x=0;x<image.bitmap.height+1;x++){
+for(var x=0;x<image.bitmap.height;x++){
     img[x]=[]
-    for(var y=0;y<image.bitmap.width+1;y++){
+    for(var y=0;y<image.bitmap.width;y++){
         //img[x].push(Jimp.intToRGBA(image.getPixelColor(x, y)))
         var tmp=Jimp.intToRGBA(image.getPixelColor(x, y))
         //console.log(img.colors.indexOf(tmp))
@@ -14,23 +15,26 @@ for(var x=0;x<image.bitmap.height+1;x++){
         img[x].push(tmp)
     }
 }
+if((x==320)&&(y==180)){
+    console.log('correct size')
 
-var pixelObj ={x:"",y:"",tile:""}
+}
+//var pixelObj ={x:"",y:"",tile:""}
 var file=[]
-Object.values(img).slice(1).forEach((imgRows,x)=>{
+Object.values(img).forEach((imgRows,x)=>{
     //console.log(imgRows)
     imgRows.forEach((pixel,y)=>{
-        pixelObj.x =x
-        pixelObj.y =y
+        //pixelObj.x =x
+        //pixelObj.y =y
         //f(Object.values(pixel).slice(0,-1)[2]==61){
         //console.log(Object.values(colors),Object.values(pixel).slice(0,-1))}
-        if(Object.values(colors).indexOf(Object.values(pixel).slice(0,-1))){
-            var color="ocean";
+
+            var tile="ocean";
             //console.log(Object.values(pixel).slice(0,-1),[0,171,61],[0,171,61]==[0,171,61],Object.values(pixel).slice(0,-1)==[0,171,61])
             if(JSON.stringify(Object.values(pixel).slice(0,-1))==JSON.stringify([0,171,61])){
-                color ="grass"
+                tile ="zcHDLCkm6n9dWR"
             }else if(JSON.stringify(Object.values(pixel).slice(0,-1))==JSON.stringify([255,255,255])){
-                color ="snow"
+                tile ="k5FQ6K4TLwdWCk"
             }
             /*Object.values(colors).forEach((colorVal,index)=>{
                 console.log(colors[Object.keys(colors)[index]],Object.values(pixel).slice(0,-1),colors[Object.keys(colors)[index]]===Object.values(pixel).slice(0,-1))
@@ -40,11 +44,19 @@ Object.values(img).slice(1).forEach((imgRows,x)=>{
             })
             pixelObj.tile= color
             //console.log(pixelObj.tile)*/
-            if(!(color=="ocean")){
-            file.push(pixelObj)}
-        }
+            if(!(tile=="ocean")){
+            file.push({x,y,texture:tile,opacity:1,tint:16777215,scale:{x:1,y:1},frame:0,rotation:0})
+
+            }
+
     })
 })
- fs.writeFileSync("./tiles",JSON.stringify(file)) 
+var game = yaml.load(fs.readFileSync(process.argv[3]))
+file.forEach((elm,index)=>{
+
+        game.rooms["0"].tiles["0"].tiles[index]=elm
+})
+
+ fs.writeFileSync(process.argv[4],yaml.dump(game))
 }
 main()
